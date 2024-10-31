a1cc33374095c35ab43015394f12d7f9d9e45797
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-31 11:05:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
 function myRace(promises) {
   // implementation
+	return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then(resolve).catch(reject);
+    });
+  });
 }
 
 function myAny(promises) {
   // implementation
+	return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then(resolve);
+    });
+
+    // If all promises reject, reject with a specific error message
+    setTimeout(() => reject("all promises rejected"), 0);
+  });
 }
 
 function myAll(promises) {
   // implementation
+	return new Promise((resolve, reject) => {
+    const results = new Array(promises.length);
+    let resolvedCount = 0;
+
+    promises.forEach((promise, index) => {
+      promise
+        .then((value) => {
+          results[index] = value;
+          resolvedCount++;
+
+          if (resolvedCount === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    });
+  });
 }
 
 function myAllSettled(promises) {
   // implementation
+	return new Promise((resolve) => {
+    const results = new Array(promises.length);
+    let settledCount = 0;
+
+    promises.forEach((promise, index) => {
+      promise
+        .then((value) => {
+          results[index] = { status: 'fulfilled', value };
+        })
+        .catch((error) => {
+          results[index] = { status: 'rejected', error };
+        })
+        .finally(() => {
+          settledCount++;
+
+          if (settledCount === promises.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
 }
 
 module.exports = {
